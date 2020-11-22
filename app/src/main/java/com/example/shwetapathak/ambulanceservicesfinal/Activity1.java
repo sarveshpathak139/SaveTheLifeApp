9c3788e39e5c805e9fcf4ceae4e4984f0223097b
sarveshpathak139 <sarveshpathak139@gmail.com>
2020-11-22 11:01:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shwetapathak/ambulanceservicesfinal/Activity1.java
@@ -0,0 +1,16 @@
+package com.example.shwetapathak.ambulanceservicesfinal;
+
+import android.provider.Settings;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+    }
+}
